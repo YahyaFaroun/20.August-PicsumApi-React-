0e0f90f0e11de1b9d1b8b65a5df7a20e4529639e
Yahya Faroun <yahyafaroun@gmail.com>
2020-09-08 15:33:50 +0200

    reload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class myPicsumApi extends Component {
+  state = {isLoading : true}
+  async componentDidMount(){
+    let array = await axios.get('https://picsum.photos/v2/list')
+    console.log(array.data)
+    this.setState({ myData: array.data });
+    this.setState({ isLoading: false });
+  }
+  render() { 
+    return (
+      <div id="boxes">
+        {this.state.isLoading ? 'Loading' :  
+
+         this.state.myData.map((elt) =>{
+         return <article id="box" key={elt.id}>
+            <img src={elt.download_url} alt=""></img>
+            <h4>{elt.author}</h4>
+            <button>See more</button>
+          </article>
+
+        })}
+        {/* <img src="https://picsum.photos/200/300" alt=""/>
+        <h4>Text</h4>
+        <button>See more</button> */}
+      </div>
+      );
+  }
 }
-
-export default App;
+ 
+export default myPicsumApi;
